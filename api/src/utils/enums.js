8a16689be11bfaee297492c9b08a59e15c6f3c20
Andre Oliveira <desmonplays@gmail.com>
2020-06-21 11:48:46 -0400

    (new feature) creating the enum shortcut

---
+++ api/src/utils/enums.js
@@ -0,0 +1,5 @@
+module.exports = {
+  pokemonTypes: [
+    'bug', 'dark', 'dragon', 'eletric', 'fairy', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water'
+  ],
+};
