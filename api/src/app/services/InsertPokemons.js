3a0d4b988a1e0f2c95815192fb9eebe05f31db55
Andre Oliveira <desmonplays@gmail.com>
2020-06-23 16:37:21 -0400

    (new feature) pusing only name of pokemon

--- api/src/app/services/InsertPokemons.js
+++ api/src/app/services/InsertPokemons.js
@@ -2,11 +2,22 @@ const fetch = require('node-fetch');
 
 const pokemonsUrl = 'https://pokeapi.co/api/v2/pokemon';
 let pokemons = [];
+let nextUrl = '';
 
 fetch(pokemonsUrl)
   .then(res => res.json())
   .then(json => {
-    pokemons = json;
+    nextUrl = json.next;
+    let pokemonsResponse = json.results;
+
+    pokemonsResponse.map(pokemonResponse => {
+      console.log(pokemonResponse.name);
+      fetch(pokemonResponse.url)
+        .then(res => res.json())
+        .then(pokemonDetails => {
+        });
+      pokemons.push(pokemonResponse.name);
+    });
   });
 
 
