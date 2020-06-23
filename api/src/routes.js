83f5cd0dbe970227d6342ae7bb4b1fd2d8c63b9f
Andre Oliveira <desmonplays@gmail.com>
2020-06-22 22:31:50 -0400

    (new feature) post on route /upload should catch multer and send the avatar, and call the filecontroller

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,17 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import PokemonsController from './app/controllers/PokemonsController';
+import FilesController from './app/controllers/FilesController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.get('/pokemons', PokemonsController.index);
 routes.post('/pokemons', PokemonsController.create);
+routes.put('/pokemons/:id', PokemonsController.update);
+
+routes.post('/upload', upload.single('avatar'), FilesController.create);
 
 export default routes;
